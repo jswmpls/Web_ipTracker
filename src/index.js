d906a8a4afba36fbc31f94ceb5f9e3bea75a92c8
jswmpls <elizavetafilipishena@yandex.ru>
2025-10-29 22:46:32 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { validateIp, addTileLayer } from "./helper";
+import icon from "../images/icon-location.svg";
+
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+});
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+  zoomControl: false,
+});
+
+addTileLayer(map);
+
+function getData() {
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://ipgeolocation.abstractapi.com/v1/?api_key=80b926ad363b4150ad2294d760607e9a&ip_address=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(setInfo);
+  }
+}
+
+function handleKey(e) {
+  if (e.key == "Enter") {
+    getData();
+  }
+}
+
+function setInfo(mapData) {
+  ipInfo.innerText = mapData.ip_address;
+  locationInfo.innerText = mapData.country + " " + mapData.region;
+  timezoneInfo.innerText = mapData.timezone.gmt_offset;
+  mapData.connection.isp_name
+    ? (ispInfo.innerText = mapData.connection.isp_name)
+    : (ispInfo.innerText = "No");
+
+  map.setView([mapData.latitude, mapData.longitude]);
+  L.marker([mapData.latitude, mapData.longitude], { icon: markerIcon }).addTo(
+    map
+  );
+}
